14aac8b089681d0a51715df50d0afc27d8a4262f
Cade Conner <55031759+realcadecon@users.noreply.github.com>
2026-09-23 19:40:29 -0500

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int L=0, R=numbers.length - 1;
+        while(L<R) {
+          int sum = numbers[L] + numbers[R];
+          if(sum == target) {
+            return new int[]{L+1, R+1};
+          } 
+          if (sum < target) L++;
+          else R--;
+        }
+        return new int[]{};
+    }
+}
